490b18c56cb6bb2cf31caf37445e493868455266
Sagar Karandikar <sagark@eecs.berkeley.edu>
2017-06-24 08:46:03 +0000

    use fpga driver

--- src/main/cc/simif_f1.cc
+++ src/main/cc/simif_f1.cc
@@ -6,20 +6,49 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+
+
 simif_f1_t::simif_f1_t() {
-  //f1_start();
+#ifdef SIMULATION_XSIM
   mkfifo(driver_to_xsim, 0666);
   printf("opening driver to xsim\n");
   driver_to_xsim_fd = open(driver_to_xsim, O_WRONLY);
   printf("opening xsim to driver\n");
   xsim_to_driver_fd = open(xsim_to_driver, O_RDONLY);
+#else
+  rc = fpga_pci_init();
+  //fail_on(rc, out, "Unable to initialize the fpga_pci library");
+
+  /* This demo works with single FPGA slot, we pick slot #0 as it works for both f1.2xl and f1.16xl */
+
+  slot_id = 0;
+
+  //rc = check_afi_ready(slot_id);
+  //fail_on(rc, out, "AFI not ready");
+
+  printf("===== Starting with peek_poke_example =====\n");
+  pci_bar_handle = PCI_BAR_HANDLE_INIT;
+  rc = fpga_pci_attach(0, FPGA_APP_PF, APP_PF_BAR0, 0, &pci_bar_handle);
+  printf("attached %d\n", rc);
+  //fail_on(rc, out, "Unable to attach to the AFI on slot id %d", slot_id);
+
+
+#endif
 }
 
 simif_f1_t::~simif_f1_t() {
   //f1_finish();
+#ifndef SIMULATION_XSIM
+        rc = fpga_pci_detach(pci_bar_handle);
+        if (rc) {
+            printf("Failure while detaching from the fpga.\n");
+        }
+#endif
 }
 
 void simif_f1_t::write(size_t addr, uint32_t data) {
+#ifdef SIMULATION_XSIM
+
     // TODO: pipe comms
     //printf("HELLO WRITE addr: %x, data %x\n");
     uint64_t cmd = ((uint64_t) 0x80000000 | addr) << 32 | (uint64_t)data;
@@ -27,9 +56,18 @@ void simif_f1_t::write(size_t addr, uint32_t data) {
     ::write(driver_to_xsim_fd, buf, 8);
     // wait for ack
     ::read(xsim_to_driver_fd, buf, 8);
+#else
+
+    rc = fpga_pci_poke(pci_bar_handle, addr >> 2, data);
+    printf("writing addr: %x, data %x, rc: %x\n", addr, data, rc);
+    //fail_on(rc, out, "Unable to write to the fpga !");
+
+
+#endif
 }
 
 uint32_t simif_f1_t::read(size_t addr) {
+#ifdef SIMULATION_XSIM
     // TODO: pipe comms
     //printf("HELLO READ addr: %x\n");
     uint64_t cmd = addr;
@@ -37,4 +75,11 @@ uint32_t simif_f1_t::read(size_t addr) {
     ::write(driver_to_xsim_fd, buf, 8);
     ::read(xsim_to_driver_fd, buf, 8);
     return *(uint64_t*)buf;
+#else
+    uint32_t value;
+    rc = fpga_pci_peek(pci_bar_handle, addr >> 2, &value);
+    printf("read addr: %x, data %x, rc: %x\n", addr, value, rc);
+    return value & 0xFFFFFFFF;
+    //fail_on(rc, out, "Unable to read read from the fpga !");
+#endif
 }
--- src/main/cc/simif_f1.h
+++ src/main/cc/simif_f1.h
@@ -4,6 +4,14 @@
 #include "simif.h"    // from midas
 //#include "f1.h" // from firesim/platforms/f1/f1.h
 
+#define SIMULATION_XSIM
+
+#ifndef SIMULATION_XSIM
+#include <fpga_pci.h>
+#include <fpga_mgmt.h>
+#endif
+
+
 class simif_f1_t: public virtual simif_t
 {
   public:
@@ -14,10 +22,16 @@ class simif_f1_t: public virtual simif_t
   private:
     char in_buf[MMIO_WIDTH];
     char out_buf[MMIO_WIDTH];
+#ifdef SIMULATION_XSIM
     char * driver_to_xsim = "/tmp/driver_to_xsim";
     char * xsim_to_driver = "/tmp/xsim_to_driver";
     int driver_to_xsim_fd;
     int xsim_to_driver_fd;
+#else
+    int rc;
+    int slot_id;
+    pci_bar_handle_t pci_bar_handle;
+#endif
 };
 
 #endif // __SIMIF_F1_H
