516493c267de3aab636177b209a727e734f3aacc
Sagar Karandikar <sagark@eecs.berkeley.edu>
2017-06-24 03:59:18 +0000

    slow, but working simif for driver to talk to xsim over pipes

--- src/main/cc/simif_f1.cc
+++ src/main/cc/simif_f1.cc
@@ -1,18 +1,40 @@
 #include "simif_f1.h"
 #include <cassert>
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 simif_f1_t::simif_f1_t() {
-  f1_start();
+  //f1_start();
+  mkfifo(driver_to_xsim, 0666);
+  printf("opening driver to xsim\n");
+  driver_to_xsim_fd = open(driver_to_xsim, O_WRONLY);
+  printf("opening xsim to driver\n");
+  xsim_to_driver_fd = open(xsim_to_driver, O_RDONLY);
 }
 
 simif_f1_t::~simif_f1_t() {
-  f1_finish();
+  //f1_finish();
 }
 
-void simif_f1_t::write(size_t addr, uint64_t data) {
-  f1_softreg_write(addr, data);
+void simif_f1_t::write(size_t addr, uint32_t data) {
+    // TODO: pipe comms
+    //printf("HELLO WRITE addr: %x, data %x\n");
+    uint64_t cmd = ((uint64_t) 0x80000000 | addr) << 32 | (uint64_t)data;
+    char * buf = (char*)&cmd;
+    ::write(driver_to_xsim_fd, buf, 8);
+    // wait for ack
+    ::read(xsim_to_driver_fd, buf, 8);
 }
 
-uint64_t simif_f1_t::read(size_t addr) {
-  return f1_softreg_read(addr);
+uint32_t simif_f1_t::read(size_t addr) {
+    // TODO: pipe comms
+    //printf("HELLO READ addr: %x\n");
+    uint64_t cmd = addr;
+    char * buf = (char*)&cmd;
+    ::write(driver_to_xsim_fd, buf, 8);
+    ::read(xsim_to_driver_fd, buf, 8);
+    return *(uint64_t*)buf;
 }
--- src/main/cc/simif_f1.h
+++ src/main/cc/simif_f1.h
@@ -2,18 +2,22 @@
 #define __SIMIF_F1_H
 
 #include "simif.h"    // from midas
-#include "f1.h" // from firesim/platforms/f1/f1.h
+//#include "f1.h" // from firesim/platforms/f1/f1.h
 
 class simif_f1_t: public virtual simif_t
 {
   public:
     simif_f1_t();
     virtual ~simif_f1_t();
-    virtual void write(size_t addr, uint64_t data);
-    virtual uint64_t read(size_t addr);
+    virtual void write(size_t addr, uint32_t data);
+    virtual uint32_t read(size_t addr);
   private:
     char in_buf[MMIO_WIDTH];
     char out_buf[MMIO_WIDTH];
+    char * driver_to_xsim = "/tmp/driver_to_xsim";
+    char * xsim_to_driver = "/tmp/xsim_to_driver";
+    int driver_to_xsim_fd;
+    int xsim_to_driver_fd;
 };
 
 #endif // __SIMIF_F1_H
