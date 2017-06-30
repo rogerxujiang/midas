13f0bcd5478a715c43e84e30ec1b7023916c36cc
Sagar Karandikar <sagark@eecs.berkeley.edu>
2017-06-30 18:24:46 +0000

    cleanup API between simif and xsim

--- src/main/cc/simif_f1.cc
+++ src/main/cc/simif_f1.cc
@@ -6,35 +6,26 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
-
 simif_f1_t::simif_f1_t() {
 #ifdef SIMULATION_XSIM
-  mkfifo(driver_to_xsim, 0666);
-  fprintf(stderr, "opening driver to xsim\n");
-  driver_to_xsim_fd = open(driver_to_xsim, O_WRONLY);
-  fprintf(stderr, "opening xsim to driver\n");
-  xsim_to_driver_fd = open(xsim_to_driver, O_RDONLY);
+    mkfifo(driver_to_xsim, 0666);
+    fprintf(stderr, "opening driver to xsim\n");
+    driver_to_xsim_fd = open(driver_to_xsim, O_WRONLY);
+    fprintf(stderr, "opening xsim to driver\n");
+    xsim_to_driver_fd = open(xsim_to_driver, O_RDONLY);
 #else
-  fprintf(stderr, "===== FPGA setup =====\n");
+    fprintf(stderr, "===== FPGA setup =====\n");
+    slot_id = 0; // TODO: make this a cmd line arg
+    int rc = fpga_pci_init();
+    check_rc(rc, "fpga_pci_init");
 
-  int rc;
-  rc = fpga_pci_init();
-  check_rc(rc, "fpga_pci_init");
+    // TODO: add back AFI checking
+    //rc = check_afi_ready(slot_id);
+    // check_rc(rc, "fpga_pci_init");
 
-  /* This demo works with single FPGA slot, we pick slot #0 as it works for both f1.2xl and f1.16xl */
-
-  slot_id = 0;
-
-  //rc = check_afi_ready(slot_id);
-  //fail_on(rc, out, "AFI not ready");
-
-  pci_bar_handle = PCI_BAR_HANDLE_INIT;
-  rc = fpga_pci_attach(0, FPGA_APP_PF, APP_PF_BAR0, 0, &pci_bar_handle);
-  check_rc(rc, "fpga_pci_attach");
-  //fail_on(rc, out, "Unable to attach to the AFI on slot id %d", slot_id);
-
-
+    pci_bar_handle = PCI_BAR_HANDLE_INIT;
+    rc = fpga_pci_attach(0, FPGA_APP_PF, APP_PF_BAR0, 0, &pci_bar_handle);
+    check_rc(rc, "fpga_pci_attach");
 #endif
 }
 
@@ -53,67 +44,37 @@ void simif_f1_t::check_rc(int rc, char * infostr) {
 
 void simif_f1_t::fpga_shutdown() {
 #ifndef SIMULATION_XSIM
-        int rc = fpga_pci_detach(pci_bar_handle);
-        // don't call check_rc because of fpga_shutdown call. do it manually:
-        if (rc) {
-            fprintf(stderr, "Failure while detaching from the fpga: %d\n", rc);
-        }
+    int rc = fpga_pci_detach(pci_bar_handle);
+    // don't call check_rc because of fpga_shutdown call. do it manually:
+    if (rc) {
+        fprintf(stderr, "Failure while detaching from the fpga: %d\n", rc);
+    }
 #endif
 }
 
 simif_f1_t::~simif_f1_t() {
-  //f1_finish();
     fpga_shutdown();
 }
 
 void simif_f1_t::write(size_t addr, uint32_t data) {
+    // addr is really a (32-byte) word address because of zynq implementation
+    addr <<= 2;
 #ifdef SIMULATION_XSIM
-
-    // TODO: pipe comms
-    //printf("HELLO WRITE addr: %x, data %x\n");
-
     uint64_t cmd = (((uint64_t)(0x80000000 | addr)) << 32) | (uint64_t)data;
     char * buf = (char*)&cmd;
     ::write(driver_to_xsim_fd, buf, 8);
     while (!is_write_ready());
-    // wait for ack
-/*    int gotdata = 0;
-    while (gotdata == 0) {
-        gotdata = ::read(xsim_to_driver_fd, buf, 8);
-        if (gotdata != 0 && gotdata != 8) {
-            printf("ERR GOTDATA %d\n", gotdata);
-        }
-    }*/
 #else
-    addr <<= 2;
-//    fprintf(stderr, "writing addr: %x, data %x\n", addr >> 2, data);
-//    __sync_synchronize();
-
     int rc = fpga_pci_poke(pci_bar_handle, addr, data);
     while (!is_write_ready());
-
-//    __sync_synchronize();
-
-//    fprintf(stderr, "writing addr: %x, data %x\n", addr >> 2, data);
-
-    //fprintf(stderr, "writing addr: %x, data %x\n", addr, data);
-//    for (int i = 0; i < 100; i++) {
-//    __sync_synchronize();
-//    }
-
-
     check_rc(rc, NULL);
-    //fail_on(rc, out, "Unable to write to the fpga !");
-
-
 #endif
 }
 
 uint32_t simif_f1_t::read(size_t addr) {
+    addr <<= 3;
 #ifdef SIMULATION_XSIM
-    // TODO: pipe comms
-    //printf("HELLO READ addr: %x\n");
-    uint64_t cmd = addr << 1;
+    uint64_t cmd = addr;
     char * buf = (char*)&cmd;
     ::write(driver_to_xsim_fd, buf, 8);
 
@@ -124,35 +85,19 @@ uint32_t simif_f1_t::read(size_t addr) {
             printf("ERR GOTDATA %d\n", gotdata);
         }
     }
-
     return *((uint64_t*)buf);
 #else
-//    while (!is_write_ready());
-
     uint32_t value;
-    addr <<= 3;
-//    fprintf(stderr, "wat\n");
-//    fprintf(stderr, "read addr: %x, data %x\n", addr >> 3, value);
-//    __sync_synchronize();
-
     int rc = fpga_pci_peek(pci_bar_handle, addr, &value);
-//    __sync_synchronize();
-
-//    fprintf(stderr, "read addr: %x, data %x\n", addr >> 3, value);
-    //check_rc(rc, NULL);
-    //fprintf(stderr, "got response\n");
-
     return value & 0xFFFFFFFF;
-    //fail_on(rc, out, "Unable to read read from the fpga !");
 #endif
 }
 
 
 uint32_t simif_f1_t::is_write_ready() {
+    uint64_t addr = 0x4;
 #ifdef SIMULATION_XSIM
-    // TODO: pipe comms
-    //printf("HELLO READ addr: %x\n");
-    uint64_t cmd = 0x1;
+    uint64_t cmd = addr;
     char * buf = (char*)&cmd;
     ::write(driver_to_xsim_fd, buf, 8);
 
@@ -163,17 +108,11 @@ uint32_t simif_f1_t::is_write_ready() {
             printf("ERR GOTDATA %d\n", gotdata);
         }
     }
-
     return *((uint64_t*)buf);
 #else
     uint32_t value;
-    uint64_t addr = 0x4;
-//    __sync_synchronize();
-//    fprintf(stderr, "wat\n");
     int rc = fpga_pci_peek(pci_bar_handle, addr, &value);
-//    __sync_synchronize();
     check_rc(rc, NULL);
     return value & 0xFFFFFFFF;
-    //fail_on(rc, out, "Unable to read read from the fpga !");
 #endif
 }
