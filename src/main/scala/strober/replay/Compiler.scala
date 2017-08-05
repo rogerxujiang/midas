e923ad9eddc7e0db7f0881833f95f0a3960b0a22
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-08-05 12:49:12 -0700

    optionally delete debug logic

--- src/main/scala/strober/replay/Compiler.scala
+++ src/main/scala/strober/replay/Compiler.scala
@@ -8,20 +8,27 @@ import firrtl.CompilerUtils.getLoweringTransforms
 import barstools.macros._
 import java.io.{File, FileWriter}
 
-private class Compiler(conf: File, json: File, lib: File, macros: File, paths: File) extends firrtl.Compiler {
+private case class CompilerArgs(
+  conf: File, json: File, lib: File, macros: File, paths: File, noDebug: Boolean) 
+private class Compiler(args: CompilerArgs) extends firrtl.Compiler {
   def transforms =
     getLoweringTransforms(ChirrtlForm, MidForm) ++
     Seq(new InferReadWrite, new ReplSeqMem) ++
     getLoweringTransforms(MidForm, LowForm) ++
-    Seq(new midas.passes.ConfToJSON(conf, json),
+    Seq(new midas.passes.ConfToJSON(args.conf, args.json),
         new MacroCompilerTransform) ++
     getLoweringTransforms(HighForm, LowForm) ++
-    Seq(new LowFirrtlOptimization)
-  def emitter = new StroberVerilogEmitter(lib, macros, paths)
+    Seq(new LowFirrtlOptimization,
+        new midas.passes.DeleteDebugLogic(args.noDebug))
+  def emitter = new StroberVerilogEmitter(args.lib, args.macros, args.paths)
 }
 
 object Compiler {
-  def apply(chirrtl: Circuit, io: chisel3.Data, dir: File, lib: Option[File]): Circuit = {
+  def apply(chirrtl: Circuit,
+            io: chisel3.Data,
+            dir: File,
+            lib: Option[File],
+            noDebug: Boolean): Circuit = {
     dir.mkdirs
     val confFile = new File(dir, s"${chirrtl.main}.conf")
     val jsonFile = new File(dir, s"${chirrtl.main}.macros.json")
@@ -33,16 +40,19 @@ object Compiler {
       MacroCompilerAnnotation(chirrtl.main, MacroCompilerAnnotation.Params(
         jsonFile.toString, lib map (_.toString), CostMetric.default, false))))
     val verilog = new FileWriter(new File(dir, s"${chirrtl.main}.v"))
-    val result = new Compiler(confFile, jsonFile, lib getOrElse jsonFile, macroFile, pathFile) compile (
-      CircuitState(chirrtl, ChirrtlForm, Some(annotations)), verilog)
+    val args = CompilerArgs(confFile, jsonFile, lib getOrElse jsonFile, macroFile, pathFile, noDebug) 
+    val result = new Compiler(args) compile (CircuitState(chirrtl, ChirrtlForm, Some(annotations)), verilog)
     genVerilogFragment(chirrtl.main, io, new FileWriter(new File(dir, s"${chirrtl.main}.vfrag")))
     verilog.close
     result.circuit
   }
 
-  def apply[T <: chisel3.Module](w: => T, dir: File, lib: Option[File] = None): Circuit = {
+  def apply[T <: chisel3.Module](w: => T,
+                                 dir: File,
+                                 lib: Option[File] = None,
+                                 noDebug: Boolean = true): Circuit = {
     lazy val dut = w
     val chirrtl = Parser.parse(chisel3.Driver.emit(() => dut))
-    apply(chirrtl, dut.io, dir, lib)
+    apply(chirrtl, dut.io, dir, lib, noDebug)
   }
 }
